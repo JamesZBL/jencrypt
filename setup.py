e65c212953e16b7ae0312a2b7454ba1ea3e703ff
JamesZBL <zhengbaole@gmail.com>
2021-02-23 14:03:13 +0800

    modify setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,16 @@
 from setuptools import setup, find_packages
 
 setup(name='jencrypt',
-      version='2.0.3',
+      version='2.0.4',
       description='File and directory encryption tool for automatically mounting data volumes',
       author='JamesZBL',
       author_email='zhengbaole@gmail.com',
       url='https://github.com/JamesZBL/jencrypt',
       packages=find_packages(),
       python_requires='>=3',
+      entry_points={
+          'console_scripts': [
+              'jencrypt = app'
+          ]
+      },
       )
