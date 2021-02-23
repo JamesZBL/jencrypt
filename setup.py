ed817ff7b815c4b44139c50866352efdbf66cd50
JamesZBL <zhengbaole@gmail.com>
2021-02-23 11:22:12 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='jencrypt',
+      version='2.0.1',
+      description='File and directory encryption tool for automatically mounting data volumes',
+      author='JamesZBL',
+      author_email='zhengbaole@gmail.com',
+      url='https://github.com/JamesZBL/jencrypt',
+      packages=['watchdog'],
+      )
