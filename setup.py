a25acd3b5902b3e801b29708784b7870c5bdc722
JamesZBL <zhengbaole@gmail.com>
2021-02-25 10:25:55 +0800

    add flit build

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python3
+import pathlib
 
 from setuptools import setup, find_packages
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
 setup(name='jencrypt',
-      version='2.0.6',
-      description='File and directory encryption tool for automatically mounting data volumes',
+      version='2.0.12',
+      description='File and directory encryption tool with automatically mounting data volumes',
+      long_description=README,
+      long_description_content_type="text/markdown",
       author='JamesZBL',
       author_email='zhengbaole@gmail.com',
+      license='Apache2',
       url='https://github.com/JamesZBL/jencrypt',
       packages=find_packages(),
+      install_requires=['watchdog'],
       python_requires='>=3',
       entry_points={
           'console_scripts': [
-              'jencrypt = app.bootstrap:main'
+              'jencrypt = jencrypt.bootstrap:main'
           ]
       },
       )
